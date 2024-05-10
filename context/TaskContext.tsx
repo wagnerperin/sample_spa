8350e451f8387c3c288085f84a2dd62f7a8ac70d
alunolab02 <alunolab02@lab.uvv>
2024-05-10 09:03:50 -0300

    updating repo

--- context/TaskContext.tsx
+++ context/TaskContext.tsx
@@ -2,27 +2,48 @@
 
 import React, {createContext, useState} from 'react';
 
+export enum Stage{
+    Backlog,
+    ToDo,
+    Doing,
+    Done
+}
+
+export type Task = {
+    description: string;
+    stage: Stage
+}
+
 type TaskContextType = {
-    tasks: string[];
+    tasks: Task[];
     addTask: (description:string) => void;
     removeTask: (taskIndex:number) => void;
+    changeStage: (taskIndex:number, new_stage:Stage) => void;
 }
 
 export const TaskContext = createContext({} as TaskContextType);
 
 export const TaskContextProvider = ({ children } : {children: React.ReactNode;}) => {
-    const [tasks, setTasks] = useState<string[]>([]);
+    const [tasks, setTasks] = useState<Task[]>([]);
 
     const addTask = (description: string) => {
-        setTasks([...tasks, description]);
+        let newTask = {
+            description,
+            stage: Stage.Backlog
+        }
+        setTasks([...tasks, newTask]);
     };
 
     const removeTask = (taskIndex:number) => {
-        setTasks(tasks.filter((_:string, index: number) => taskIndex !== index));
+        setTasks(tasks.filter((_:Task, index: number) => taskIndex !== index));
+    };
+
+    const changeStage = (taskIndex:number, new_stage:Stage) => {
+        tasks[taskIndex].stage = new_stage;
     };
 
     return (
-        <TaskContext.Provider value={{ tasks, addTask, removeTask }}>
+        <TaskContext.Provider value={{ tasks, addTask, removeTask, changeStage }}>
             {children}
         </TaskContext.Provider>
     );
